254d2b8f9f609d533f22cf27a48d71f6170d630c
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2021-08-02 22:13:42 +0200

    completed rate_guess() function

--- R/rate_guess.R
+++ R/rate_guess.R
@@ -1,7 +1,6 @@
 #' Rate a guess, given a set of available combinations
 #'
 #' @param guess numeric vector with a particular guess
-#' @param all_combos matrix of all possible combinations, eg, colors^pegs
 #' @param remaining_combos matrix of all remaining available combinations
 #'
 #' @return integer. Maximum number of combinations we
@@ -9,9 +8,22 @@
 #' @export
 #'
 #' @examples
-#' co <- combos(2, 2)
-#' rate_guess(all_combos = co, remaining_combos = co, guess = c(1, 1))
-rate_guess <- function(all_combos, remaining_combos, guess) {
-  possible_responses <- apply(remaining_combos, 1, response, guess)
-  return(possible_responses)
+#' rate_guess(remaining_combos = combos(2, 2), guess = c(1, 1))
+rate_guess <- function(remaining_combos, guess, FUN = max, ...) {
+
+  # Get all possible responses (no of blacks and whites) from guess
+  all_responses <- apply(remaining_combos, 1, response, guess) %>% t()
+
+  # We only need to consider *unique* responses
+  all_responses <- unique(all_responses)
+
+  # Convert to list of vectors
+  possible_responses <- lapply(seq_len(nrow(all_responses)), function(i) all_responses[i,])
+
+  # Find out how many combos are left after each possible response to guess
+  n_combos <- possible_responses %>%
+    map_int(~ reduce_combos(remaining_combos, guess, .x) %>% nrow())
+
+  # Summarise: Default is to return max number of remaining combos after guess
+  return(FUN(n_combos, ...))
 }
