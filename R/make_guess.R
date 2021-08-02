d86ca0b85f0fd6d8203fcb344751449b26c25dd5
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2021-08-02 23:34:31 +0200

    completed make_guess() function and minor edits

--- R/make_guess.R
+++ R/make_guess.R
@@ -1,13 +1,26 @@
 #' Makes a guess a the secret code
 #'
-#' @param combos available combinations to guess at
+#' @param all_combos tibble with all combinations to pick a guess from
+#' @param remaining_combos tibble with available combinations to guess at
 #'
 #' @return numeric vector representing a valid guess at secret code
 #' @export
 #'
 #' @examples
-#' make_guess(combos = matrix(c(1,1,2,2,1,2,1,2), 4))
+#' co <- combos(2, 2)
+#' make_guess(all_combos = co, remaining_combos = co)
 #'
-make_guess <- function(combos) {
+make_guess <- function(all_combos, remaining_combos) {
 
+  # Convert to list of vectors
+  possible_guesses <- lapply(seq_len(nrow(all_combos)), function(i) as.matrix(all_combos)[i, ])
+
+  # Rate every possible guess based on every possible response
+  ratings <- possible_guesses %>%
+    purrr::map_int(~ rate_guess(remaining_combos, .x))
+
+  # Find id of guesses with the lowest guaranteed remaining combos
+  best_guesses <- which(ratings == min(ratings))
+
+  return(possible_guesses[[best_guesses[1]]])
 }
--- R/rate_guess.R
+++ R/rate_guess.R
@@ -2,9 +2,14 @@
 #'
 #' @param guess numeric vector with a particular guess
 #' @param remaining_combos matrix of all remaining available combinations
+#' @param FUN function used to summarise the number of remaining combinations
+#'     after a guess followed by a response
+#' @param ... parameters to be passed on to FUN if any
 #'
-#' @return integer. Maximum number of combinations we
-#' can end up with after making this guess.
+#' @return Summary of the number of combinations we can end up with
+#'     after making this guess. Default is max(). So, after guessing
+#'     at a code like 1234, any response will leave us with at
+#'     \emph{most} this number of remaining possible combinations.
 #' @export
 #'
 #' @examples
@@ -18,11 +23,11 @@ rate_guess <- function(remaining_combos, guess, FUN = max, ...) {
   all_responses <- unique(all_responses)
 
   # Convert to list of vectors
-  possible_responses <- lapply(seq_len(nrow(all_responses)), function(i) all_responses[i,])
+  possible_responses <- lapply(seq_len(nrow(all_responses)), function(i) all_responses[i, ])
 
   # Find out how many combos are left after each possible response to guess
   n_combos <- possible_responses %>%
-    map_int(~ reduce_combos(remaining_combos, guess, .x) %>% nrow())
+    purrr::map_int(~ reduce_combos(remaining_combos, guess, .x) %>% nrow())
 
   # Summarise: Default is to return max number of remaining combos after guess
   return(FUN(n_combos, ...))
